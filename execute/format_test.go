24ef3526ce3eac82b9bd87c6537087d77d4b6f68
mbellotti <marianne.bellotti@gmail.com>
2022-07-22 21:52:01 -0400

    some more tests

--- execute/format_test.go
+++ execute/format_test.go
@@ -64,3 +64,61 @@ func TestPhis(t *testing.T) {
 		t.Fatal("phi at index 7 not was removed")
 	}
 }
+
+func TestMultiPhis(t *testing.T) {
+	test := make(map[string]Scenario)
+	test["test_value"] = &FloatTrace{
+		results: map[int16]float64{0: 1.0, 1: 1.5, 2: 2.3, 3: 4.0, 4: 2.3, 5: 3.4, 6: 3.8, 7: 5.0, 8: 5.2, 9: 3.8},
+		weights: map[int16]float64{},
+	}
+
+	phis := []smt.Fork{{
+		"test_value": []*smt.Choice{
+			{
+				Base:   "test_value",
+				Values: []int16{1, 2},
+			},
+			{
+				Base:   "test_value",
+				Values: []int16{3},
+			},
+		}},
+		{
+			"test_value": []*smt.Choice{
+				{
+					Base:   "test_value",
+					Values: []int16{5, 6},
+				},
+				{
+					Base:   "test_value",
+					Values: []int16{7, 8},
+				},
+			}},
+	}
+
+	mc := NewModelChecker()
+	mc.LoadMeta(phis)
+
+	v := deadBranches("test_value", test["test_value"], mc.forks)
+	if _, ok := v.(*FloatTrace).Index(3); ok {
+		t.Fatal("value at index 3 of variable test_value was not removed")
+	}
+	if _, ok := v.(*FloatTrace).Index(1); !ok {
+		t.Fatal("value at index 1 of variable test_value was removed")
+	}
+
+	if _, ok := v.(*FloatTrace).Index(4); ok {
+		t.Fatal("phi at index 4 not was removed")
+	}
+
+	if _, ok := v.(*FloatTrace).Index(6); !ok {
+		t.Fatal("value at index 6 of variable test_value was removed")
+	}
+
+	if _, ok := v.(*FloatTrace).Index(8); ok {
+		t.Fatal("value at index 8 of variable test_value was not removed")
+	}
+	if _, ok := v.(*FloatTrace).Index(9); ok {
+		t.Fatal("phi at index 9 not was removed")
+	}
+}
