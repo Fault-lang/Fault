07317ca2822c82547b1e4320947386bdadfb282f
mbellotti <marianne.bellotti@gmail.com>
2023-02-22 22:44:59 -0500

    updating command line interface to be consistent across Docker and main.go

--- main.go
+++ main.go
@@ -202,10 +202,10 @@ func main() {
 	var input string
 	var filepath string
 	var reach bool
-	modeCommand := flag.String("mode", "check", "stop compiler at certain milestones: ast, ir, smt, or check")
-	inputCommand := flag.String("input", "fspec", "format of the input file (default: fspec)")
-	fpCommand := flag.String("filepath", "", "path to file to compile")
-	reachCommand := flag.String("complete", "false", "make sure the transitions to all defined states are specified in the model")
+	modeCommand := flag.String("m", "check", "stop compiler at certain milestones: ast, ir, smt, or check")
+	inputCommand := flag.String("i", "fspec", "format of the input file (default: fspec)")
+	fpCommand := flag.String("f", "", "path to file to compile")
+	reachCommand := flag.String("c", "false", "make sure the transitions to all defined states are specified in the model")
 
 	flag.Parse()
 
