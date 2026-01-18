ac387b69bd310719abfda138d0cb9a63da95b3fa
mbellotti <marianne.bellotti@gmail.com>
2026-01-17 22:04:04 -0500

    bug: dropping inits for phis in parallels

--- generator/unpack/unpack.go
+++ generator/unpack/unpack.go
@@ -589,6 +589,7 @@ func (u *Unpacker) unpackParallel(p *rules.Parallels) ([]*rules.Init, string) {
 
 		phis = append(phis, PhiClone)
 		inits, caps, _ := u.buildPhis(phis, nil)
+		u.AddInit(inits)
 		SelectorRule := u.Log.NewBranchSelector(u.CurrentBlock, i, caps, InitsToList((inits)))
 		rule_set = append(rule_set, fmt.Sprintf("(assert %s)", SelectorRule.WriteRule()))
 		u.Log.AddBranchSelector(SelectorRule)
