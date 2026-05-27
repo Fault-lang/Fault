eef6bc08e019c28215cad865674763e88b7c5cbf
mbellotti <marianne.bellotti@gmail.com>
2026-05-27 08:43:38 -0400

    removing weights now that uncertain translate to rules

--- execute/history.go
+++ execute/history.go
@@ -25,13 +25,11 @@ type FloatTrace struct {
 	Scenario
 	Base    string
 	results map[int16]float64
-	weights map[int16]float64
 }
 
 func NewFloatTrace() *FloatTrace {
 	return &FloatTrace{
 		results: make(map[int16]float64),
-		weights: make(map[int16]float64),
 	}
 }
 
@@ -44,34 +42,23 @@ func (ft *FloatTrace) Get() map[int16]float64 {
 	return ft.results
 }
 
-func (ft *FloatTrace) GetWeights() map[int16]float64 {
-	return ft.weights
-}
-
 func (ft *FloatTrace) Add(i int16, f float64) {
 	ft.results[i] = f
 }
 
-func (ft *FloatTrace) AddWeight(i int16, f float64) {
-	ft.weights[i] = f
-}
-
 func (ft *FloatTrace) Remove(i int16) {
 	delete(ft.results, i)
-	delete(ft.weights, i)
 }
 
 type IntTrace struct {
 	Scenario
 	Base    string
 	results map[int16]int64
-	weights map[int16]float64
 }
 
 func NewIntTrace() *IntTrace {
 	return &IntTrace{
 		results: make(map[int16]int64),
-		weights: make(map[int16]float64),
 	}
 }
 
@@ -84,34 +71,23 @@ func (it *IntTrace) Get() map[int16]int64 {
 	return it.results
 }
 
-func (it *IntTrace) GetWeights() map[int16]float64 {
-	return it.weights
-}
-
 func (it *IntTrace) Add(i int16, in int64) {
 	it.results[i] = in
 }
 
-func (it *IntTrace) AddWeight(i int16, f float64) {
-	it.weights[i] = f
-}
-
 func (it *IntTrace) Remove(i int16) {
 	delete(it.results, i)
-	delete(it.weights, i)
 }
 
 type BoolTrace struct {
 	Scenario
 	Base    string
 	results map[int16]bool
-	weights map[int16]float64
 }
 
 func NewBoolTrace() *BoolTrace {
 	return &BoolTrace{
 		results: make(map[int16]bool),
-		weights: make(map[int16]float64),
 	}
 }
 
@@ -124,19 +100,10 @@ func (bt *BoolTrace) Get() map[int16]bool {
 	return bt.results
 }
 
-func (bt *BoolTrace) GetWeights() map[int16]float64 {
-	return bt.weights
-}
-
 func (bt *BoolTrace) Add(i int16, b bool) {
 	bt.results[i] = b
 }
 
-func (bt *BoolTrace) AddWeight(i int16, f float64) {
-	bt.weights[i] = f
-}
-
 func (bt *BoolTrace) Remove(i int16) {
 	delete(bt.results, i)
-	delete(bt.weights, i)
 }
